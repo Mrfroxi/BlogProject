7f454a3efae410f83a6599f61f69c88eba292c92
Mrfroxi <pilutikas340@gmail.com>
2025-12-17 15:03:26 +0300

    fix: fix validation trim

--- src/blogs/validators/blog-update.validator.ts
+++ src/blogs/validators/blog-update.validator.ts
@@ -3,22 +3,21 @@ import {body} from "express-validator";
 const webRegex =  '^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$'
 
 const blogNameValidator = body('name')
+    .trim()
     .notEmpty().withMessage('name is empty')
     .isString().withMessage('name must be a string')
-    .trim()
     .isLength({ max: 15 }).withMessage('Length of name is not correct')
 
 const blogDescriptionValidator = body('description')
+    .trim()
     .notEmpty().withMessage('Description is empty')
     .isString().withMessage('Description must be a string')
-    .trim()
     .isLength({ max: 500 }).withMessage('Description of name is not correct')
 
 
 const blogWebsiteUrlValidator = body('websiteUrl')
     .notEmpty().withMessage('websiteUrl is empty')
     .isString().withMessage('websiteUrl must be a string')
-    .trim()
     .isLength({ max: 100 }).withMessage('websiteUrl of name is not correct')
     .matches(webRegex).withMessage('Regex')
 
--- src/posts/validators/post-create.validator.ts
+++ src/posts/validators/post-create.validator.ts
@@ -1,21 +1,21 @@
 import {body} from "express-validator";
 
 const titleValidator = body('title')
+    .trim()
     .notEmpty().withMessage('title must be')
     .isString().withMessage('title must be a string')
-    .trim()
     .isLength({max:30}).withMessage('title max length 30 symbols')
 
 const shortDescriptionValidator = body('shortDescription')
+    .trim()
     .notEmpty().withMessage('shortDescription must be')
     .isString().withMessage('shortDescription must be a string')
-    .trim()
     .isLength({max:100}).withMessage('shortDescription max length 100 symbols')
 
 const contentValidator = body('content')
+    .trim()
     .notEmpty().withMessage('content must be')
     .isString().withMessage('content must be a string')
-    .trim()
     .isLength({max:100}).withMessage('content max length 30 symbols')
 
 const blogIdValidator = body('blogId')
