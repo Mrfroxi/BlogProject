8228498b3eb84f15558a85d31c00d2fe99d1ca6d
Mrfroxi <pilutikas340@gmail.com>
2025-12-24 21:00:00 +0300

    fix:fix blog-router

--- src/blogs/routers/blogs.router.ts
+++ src/blogs/routers/blogs.router.ts
@@ -9,12 +9,17 @@ import {SuperAdminGuard} from "../../auth/middleware/super-admin.guard-middlewar
 import {updateBlogHandler} from "./handlers/update-blog.handler";
 import {blogUpdateValidator} from "../validators/blog-update.validator";
 import {deleteBlogHandler} from "./handlers/delete-blog.handler";
+import {paginationSortingValidator} from "../../core/middlewares/validation/pagination.sorting.validator";
+import {DriverSortField} from "../../core/types/driver-sortField";
 
 export const blogsRouter = Router({});
 
 
 blogsRouter
-    .get('', getBlogsListHandler)
+    .get('',
+        paginationSortingValidator(DriverSortField),
+        inputValidationResultMiddleware,
+        getBlogsListHandler)
     .get('/:id',idParamValidator, inputValidationResultMiddleware,
          getBlogHandler)
     .post('',SuperAdminGuard,blogCreateValidator,inputValidationResultMiddleware ,
