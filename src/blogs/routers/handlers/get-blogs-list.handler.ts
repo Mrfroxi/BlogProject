457479e29852f40d2a97c0b29a17fa8b1029350d
Mrfroxi <pilutikas340@gmail.com>
2025-12-29 21:00:00 +0300

    feat:add get-blogs-list

--- src/blogs/routers/handlers/get-blogs-list.handler.ts
+++ src/blogs/routers/handlers/get-blogs-list.handler.ts
@@ -1,22 +1,39 @@
-import {Request, Response} from "express";
 import {blogsRepository} from "../../repositories/blogs.repository";
 import {Blog} from "../../types/blog";
 import {HttpStatuses} from "../../../core/types/http-statuses";
 import {mapBlogsListToOutput} from "../mappers/map-blogs-list-to-output";
 import {WithId} from "mongodb";
 
-export async function getBlogsListHandler(_req:Request, res:Response){
+import { Request, Response } from 'express';
+import {matchedData} from "express-validator";
 
-    try{
-        const blogs: WithId<Blog>[] =  await blogsRepository.findAll()
-
-        res.status(HttpStatuses.Ok).send(mapBlogsListToOutput(blogs));
-
-    }catch (e:unknown){
-
-        res.sendStatus(HttpStatuses.InternalServerError)
-
+export async function getBlogsListHandler(
+    req: Request<{}, {}, {}>,
+    res: Response,
+) {
+    try {
+        const sanitizedQuery = matchedData(req, {
+            locations: ['query'],
+            includeOptionals: true,//include optional fields even if they are not sent
+        });
+        res.send('ok');
+    } catch (e) {
+        res.status(500).send('error');
     }
-
-
 }
+
+// export async function getBlogsListHandler(_req:Request, res:Response){
+//
+//     try{
+//         const blogs: WithId<Blog>[] =  await blogsRepository.findAll()
+//
+//         res.status(HttpStatuses.Ok).send(mapBlogsListToOutput(blogs));
+//
+//     }catch (e:unknown){
+//
+//         res.sendStatus(HttpStatuses.InternalServerError)
+//
+//     }
+//
+//
+// }
