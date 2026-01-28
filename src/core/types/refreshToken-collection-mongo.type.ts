22a71f44d68e6047bf8129d256b806595f818fb4
Mrfroxi <pilutikas340@gmail.com>
2026-01-28 14:00:00 +0300

    feat:add refreshToken collection

---
+++ src/core/types/refreshToken-collection-mongo.type.ts
@@ -0,0 +1,7 @@
+
+
+
+export type RefreshTokenCollection = {
+    userId:string,
+    refreshTokens: Record<string,boolean>,
+}
--- src/db/mongo.db.ts
+++ src/db/mongo.db.ts
@@ -4,19 +4,21 @@ import {Blog} from "../entities/blogs/types/blog";
 import {Post} from "../entities/posts/types/post";
 import {User} from "../entities/user/types/user";
 import {Comment} from "../entities/comments/types/comment";
+import {RefreshTokenCollection} from "../core/types/refreshToken-collection-mongo.type";
 
 
 const BLOGS_COLLECTION_NAME = 'blogs';
 const POSTS_COLLECTION_NAME = 'posts';
 const USERS_COLLECTION_NAME = 'users';
 const COMMENTS_COLLECTION_NAME = 'comments';
+const REFRESH_TOKEN_COLLECTION_NAME = 'refreshTokenCollection';
 
 export let client: MongoClient;
 export let blogCollection: Collection<Blog>;
 export let postCollection: Collection<Post>;
 export let userCollection: Collection<User>;
 export let commentCollection: Collection<Comment>;
-
+export let refreshTokenCollection: Collection<RefreshTokenCollection>;
 
 export async function runDB(url: string): Promise<void> {
     client = new MongoClient(url);
@@ -26,6 +28,7 @@ export async function runDB(url: string): Promise<void> {
     postCollection = db.collection<Post>(POSTS_COLLECTION_NAME);
     userCollection = db.collection<User>(USERS_COLLECTION_NAME);
     commentCollection = db.collection<Comment>(COMMENTS_COLLECTION_NAME);
+    refreshTokenCollection = db.collection<RefreshTokenCollection>(REFRESH_TOKEN_COLLECTION_NAME);
 
     try {
         await client.connect();
