b3ead3d86f40abdb2936831829bf5087c35e443f
Mrfroxi <pilutikas340@gmail.com>
2026-02-19 03:13:04 +0300

    feat:mock nodemailer in the tests

--- __tests__/auth.e2e.test.ts
+++ __tests__/auth.e2e.test.ts
@@ -4,10 +4,13 @@ import { userCollection } from '../src/db/mongo.db';
 import { takeCodeByCreateMockUser } from './helpers/codeConfirmation.helper';
 import { container } from '../src/composition-root';
 import { UserRepository } from '../src/entities/user/repositories/user.repository';
+import { nodemailerService } from '../src/core/services/nodemailerService';
 
 describe('auth endpoint', () => {
   const app = createTestApp();
   const userRepository = container.get<UserRepository>(UserRepository);
+
+  jest.spyOn(nodemailerService, 'sendEmail').mockResolvedValue(true);
 
   it('main endPoint', async () => {
     await request(app).get('/').expect(200);
